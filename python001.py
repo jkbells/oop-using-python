b337ade047fa3c4d9c5a97d1140f41fad6cfa0ba
jawadkhan317 <73393333+jawadkhan317@users.noreply.github.com>
2021-01-14 19:56:29 +0500

    classes

---
+++ python001.py
@@ -0,0 +1,22 @@
+# classes in python
+
+class Animal:
+    legs=False
+    name=''
+    
+    def sound(self):
+        return '*silence*'
+    def move(self):
+        return True
+        
+        a = Animal()
+        
+        a.legs
+        
+        a.name
+        
+        a.sound()
+        a.move()
+        
+        
+        
